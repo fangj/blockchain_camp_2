36b2611b560fb5a540abe1ba57bff9796c7b48fb
Fang Jian <fangjianmail@126.com>
2023-03-20 13:44:43 +0800

    w1-2 test ok

--- w1-2/w1_hardhat/contracts/Counter.sol
+++ w1-2/w1_hardhat/contracts/Counter.sol
@@ -3,12 +3,16 @@ pragma solidity ^0.8.9;
 
 contract Counter{
     uint public counter;
+    address owner;
 
     constructor(){
+        owner=msg.sender;
         counter=0;
     }
 
+    //仅有部署者 可以调⽤ count();
     function count() public {
+        require(msg.sender==owner,"only owner can count");
         counter++;
     }
 
---
+++ w1-2/w1_hardhat/test/Counter.js
@@ -0,0 +1,36 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+  describe("Counter",function(){
+    let counter;
+    let owner,otherAccount;
+
+    async function init(){
+        [owner,otherAccount]=await ethers.getSigners();
+        const Counter = await ethers.getContractFactory("Counter");
+        counter =await Counter.deploy();
+        await counter.deployed();
+        console.log("counter:"+counter.address);
+    }
+
+    before(async function(){
+        await init();
+    })
+
+    it("init equal 0",async function(){
+        expect(await counter.counter()).to.equal(0);
+    })
+
+    it("owner can count",async function(){
+        const counter_before=await counter.counter();
+        const tx=await counter.count();
+        await tx.wait();
+        expect (await counter.counter()).to.equal(counter_before+1);
+    })
+
+    it("owner cann't count",async function(){
+        const counter_other=await counter.connect(otherAccount);
+        expect (counter_other.count()).to.be.revertedWith("only owner can count");
+    })
+
+  })
